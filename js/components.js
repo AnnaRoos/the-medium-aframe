97e54b0e2aa2133cd24cc480451697fbf266032c
Anna Roos <anna.flytstrom@gmail.com>
2021-06-07 13:17:14 +0200

    added last message

--- js/components.js
+++ js/components.js
@@ -173,61 +173,61 @@ AFRAME.registerComponent('tarot', {
   update: function () {
     const el = this.el;
     const data = this.data;
-    /*     el.sceneEl.addEventListener('flying', function () { */
-    el.addEventListener('click', function (event) {
-      const tarotCard1 = document.createElement('a-plane');
-      tarotCard1.setAttribute('id', 'tarotcard-1');
-      tarotCard1.setAttribute('src', '#tarot-1');
-      tarotCard1.setAttribute('height', '0.5');
-      tarotCard1.setAttribute('width', '0.3');
-      tarotCard1.setAttribute('position', '4 1 0');
-      tarotCard1.setAttribute('rotation', '-90 -90 0');
-      el.sceneEl.appendChild(tarotCard1);
+    el.sceneEl.addEventListener('flying', function () {
+      el.addEventListener('click', function (event) {
+        const tarotCard1 = document.createElement('a-plane');
+        tarotCard1.setAttribute('id', 'tarotcard-1');
+        tarotCard1.setAttribute('src', '#tarot-1');
+        tarotCard1.setAttribute('height', '0.5');
+        tarotCard1.setAttribute('width', '0.3');
+        tarotCard1.setAttribute('position', '4 1 0');
+        tarotCard1.setAttribute('rotation', '-90 -90 0');
+        el.sceneEl.appendChild(tarotCard1);
 
-      const tarotCard2 = document.createElement('a-plane');
-      tarotCard2.setAttribute('id', 'tarotcard-2');
-      tarotCard2.setAttribute('src', '#tarot-2');
-      tarotCard2.setAttribute('height', '0.5');
-      tarotCard2.setAttribute('width', '0.3');
-      tarotCard2.setAttribute('position', '4 1 0.5');
-      tarotCard2.setAttribute('rotation', '-90 -90 0');
-      el.sceneEl.appendChild(tarotCard2);
+        const tarotCard2 = document.createElement('a-plane');
+        tarotCard2.setAttribute('id', 'tarotcard-2');
+        tarotCard2.setAttribute('src', '#tarot-2');
+        tarotCard2.setAttribute('height', '0.5');
+        tarotCard2.setAttribute('width', '0.3');
+        tarotCard2.setAttribute('position', '4 1 0.5');
+        tarotCard2.setAttribute('rotation', '-90 -90 0');
+        el.sceneEl.appendChild(tarotCard2);
 
-      const tarotCard3 = document.createElement('a-plane');
-      tarotCard3.setAttribute('id', 'tarotcard-3');
-      tarotCard3.setAttribute('src', '#tarot-3');
-      tarotCard3.setAttribute('height', '0.5');
-      tarotCard3.setAttribute('width', '0.3');
-      tarotCard3.setAttribute('position', '4 1 1');
-      tarotCard3.setAttribute('rotation', '-90 -90 0');
-      el.sceneEl.appendChild(tarotCard3);
+        const tarotCard3 = document.createElement('a-plane');
+        tarotCard3.setAttribute('id', 'tarotcard-3');
+        tarotCard3.setAttribute('src', '#tarot-3');
+        tarotCard3.setAttribute('height', '0.5');
+        tarotCard3.setAttribute('width', '0.3');
+        tarotCard3.setAttribute('position', '4 1 1');
+        tarotCard3.setAttribute('rotation', '-90 -90 0');
+        el.sceneEl.appendChild(tarotCard3);
 
-      const qrCode = document.createElement('a-image');
-      qrCode.setAttribute('id', 'qr-code');
-      qrCode.setAttribute('src', '#qr');
-      qrCode.setAttribute('scale', '0.4 0.4 0.4');
-      qrCode.setAttribute('position', '4.19 2 1.4');
-      qrCode.setAttribute('rotation', '0 90 0');
-      qrCode.setAttribute('flip-card', {
-        card1: '#tarotcard-1',
-        card2: '#tarotcard-2',
-        card3: '#tarotcard-3',
-        marker1: '#marker-1',
-        marker2: '#marker-2',
-        marker3: '#marker-3',
-        camRig: '#camera-rig'
+        const qrCode = document.createElement('a-image');
+        qrCode.setAttribute('id', 'qr-code');
+        qrCode.setAttribute('src', '#qr');
+        qrCode.setAttribute('scale', '0.4 0.4 0.4');
+        qrCode.setAttribute('position', '4.19 2 1.4');
+        qrCode.setAttribute('rotation', '0 90 0');
+        qrCode.setAttribute('flip-card', {
+          card1: '#tarotcard-1',
+          card2: '#tarotcard-2',
+          card3: '#tarotcard-3',
+          marker1: '#marker-1',
+          marker2: '#marker-2',
+          marker3: '#marker-3',
+          camRig: '#camera-rig',
+          endMessage: '#end-message',
+        });
+        el.sceneEl.appendChild(qrCode);
 
+        data.message1.object3D.visible = false;
+        data.message2.object3D.visible = true;
+        data.text.setAttribute(
+          'value',
+          'Follow the cards! \n Have a PLEASANT trip.'
+        );
       });
-      el.sceneEl.appendChild(qrCode);
-
-      data.message1.object3D.visible = false;
-      data.message2.object3D.visible = true;
-      data.text.setAttribute(
-        'value',
-        'Scan the code and click the cards. \n Have a pleasant trip!'
-      );
     });
-    /*     }); */
   },
 });
 
@@ -239,27 +239,30 @@ AFRAME.registerComponent('flip-card', {
     marker1: { type: 'selector', default: '' },
     marker2: { type: 'selector', default: '' },
     marker3: { type: 'selector', default: '' },
-    camRig: {type: 'selector', default: ''}
+    camRig: { type: 'selector', default: '' },
+    endMessage: { type: 'selector', default: '' },
   },
   update: function () {
     const el = this.el;
     const data = this.data;
 
     data.card1.addEventListener('click', function () {
-      /*       data.marker1.object3D.visible = true; */
+      data.marker1.object3D.visible = true;
       data.card1.object3D.visible = false;
       data.card2.addEventListener('click', function () {
-        /*         data.marker2.object3D.visible = true; */
+        data.marker2.object3D.visible = true;
         data.card2.object3D.visible = false;
         data.card3.addEventListener('click', function () {
-          /*           data.marker3.object3D.visible = true; */
+          data.marker3.object3D.visible = true;
           data.card3.object3D.visible = false;
-          data.camRig.setAttribute(
-            'movement-controls', {constrainToNavMesh: false}
-          );
+          data.camRig.setAttribute('movement-controls', {
+            constrainToNavMesh: false,
+          });
+          data.endMessage.addEventListener('click', function () {
+            window.parent.postMessage('nextLevel');
+          });
         });
       });
     });
   },
 });
-
